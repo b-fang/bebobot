332f4c275d95c0db33533515f3fde27701c91593
Brian Fang <brianfang@ucla.edu>
2022-10-03 10:28:43 -0700

    added images for ranked emblems, used league-v4 to find ranked information from api

--- beboBot.py
+++ beboBot.py
@@ -17,7 +17,7 @@ riotApiKey = config.api_key
 watcherLol = LolWatcher(riotApiKey)
 
 discordToken = config.discordToken
-discordGuild = config.discordGuild
+
 load_dotenv()
 TOKEN = os.getenv(discordToken)
 
@@ -30,13 +30,17 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 prv = 'NA1'
 summonerName = 'b3b0'
 
+
 summoner = watcherLol.summoner.by_name(prv, summonerName)
 
-print(summoner)
+my_ranked_stats = watcherLol.league.by_summoner(prv, summoner['id'])
+print(my_ranked_stats)
 
-@bot.command(name='bebo', help="gives bebo's current rank")
+@bot.command(name='beboLOL', help="gives bebo's current rank in solo/duo")
 async def sendMessage(ctx):
     response = summoner['summonerLevel']
     await ctx.send(response)
+
+#@bot.command(name='beboTFT', help="gives bebo's current rank in TFT")
 
 bot.run(discordToken)
