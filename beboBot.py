9d91bfcb8fd7a0798a5ea3b34c7c6578652b6fb7
Brian Fang <brianfang@ucla.edu>
2022-10-03 08:56:18 -0700

    first test of getting summoner information from api

--- beboBot.py
+++ beboBot.py
@@ -1,5 +1,6 @@
 # imports
 
+from cgi import print_form
 from riotwatcher import LolWatcher , TftWatcher
 import config
 
@@ -8,4 +9,10 @@ import config
 riotApiKey = config.api_key
 watcherLol = LolWatcher(riotApiKey)
 
+#platform routing value
+prv = 'NA1'
+summonerName = 'b3b0'
 
+summoner = watcherLol.summoner.by_name(prv, summonerName)
+
+print(summoner)
