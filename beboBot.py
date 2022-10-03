7c333de65d16309d7702dd465af4e27d1209fb22
Brian Fang <brianfang@ucla.edu>
2022-10-03 16:08:12 -0700

    added functionality for embedded bot message

--- beboBot.py
+++ beboBot.py
@@ -30,17 +30,32 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 prv = 'NA1'
 summonerName = 'b3b0'
 
-
 summoner = watcherLol.summoner.by_name(prv, summonerName)
 
 my_ranked_stats = watcherLol.league.by_summoner(prv, summoner['id'])
+
+counter = 0
+
+LP = my_ranked_stats[0].get('leaguePoints')
+Rank = my_ranked_stats[0].get('tier')
+
 print(my_ranked_stats)
 
-@bot.command(name='beboLOL', help="gives bebo's current rank in solo/duo")
+@bot.command(name='beboLOL')
 async def sendMessage(ctx):
     response = summoner['summonerLevel']
     await ctx.send(response)
 
 #@bot.command(name='beboTFT', help="gives bebo's current rank in TFT")
+@bot.command(name='test')
+async def sendMessage(ctx):
+    embed = discord.Embed(
+        title="B3B0 Rank Tracker"
+    )
+    embed.add_field(
+        name='Current Rank',
+        value= str(Rank) + ' : ' + str(LP), 
+    )
+    await ctx.send(embed=embed)
 
 bot.run(discordToken)
