1b07a45693186c2bb6ee1b761aef0d1fd9f3e36e
Brian Fang <brianfang@ucla.edu>
2022-10-03 09:48:11 -0700

    sucessfully tested bot in private channel to display summoner level

--- beboBot.py
+++ beboBot.py
@@ -8,7 +8,7 @@ import os
 import discord
 from dotenv import load_dotenv
 
-
+from discord.ext import commands
 
 
 # initialize
@@ -20,9 +20,11 @@ discordToken = config.discordToken
 discordGuild = config.discordGuild
 load_dotenv()
 TOKEN = os.getenv(discordToken)
-GUILD = os.getenv(discordGuild)
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.all()
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 #platform routing value
 prv = 'NA1'
@@ -32,13 +34,9 @@ summoner = watcherLol.summoner.by_name(prv, summonerName)
 
 print(summoner)
 
+@bot.command(name='bebo', help="gives bebo's current rank")
+async def sendMessage(ctx):
+    response = summoner['summonerLevel']
+    await ctx.send(response)
 
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
-    print(summoner['summonerLevel'])
-
-client.run(discordToken)
+bot.run(discordToken)
