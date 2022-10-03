3df7bd388b176f5aab9bea8c8d61631632b5bc1b
Brian Fang <brianfang@ucla.edu>
2022-10-03 08:47:02 -0700

    hid api key through config file

--- beboBot.py
+++ beboBot.py
@@ -1,10 +1,11 @@
 # imports
 
 from riotwatcher import LolWatcher , TftWatcher
+import config
 
 # initialize
 
-riotApiKey = RGAPI-f082516c-2fd1-4355-8e7f-1f320d53ad69
+riotApiKey = config.api_key
 watcherLol = LolWatcher(riotApiKey)
 
 
