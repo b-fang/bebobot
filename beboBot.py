2c4a4ffe6ea28030d784eff4697cc9e89a15a7e4
Brian Fang <brianfang@ucla.edu>
2022-10-03 09:25:44 -0700

    connected with discord api, tested with output of summonerName

--- beboBot.py
+++ beboBot.py
@@ -4,10 +4,25 @@ from cgi import print_form
 from riotwatcher import LolWatcher , TftWatcher
 import config
 
+import os
+import discord
+from dotenv import load_dotenv
+
+
+
+
 # initialize
 
 riotApiKey = config.api_key
 watcherLol = LolWatcher(riotApiKey)
+
+discordToken = config.discordToken
+discordGuild = config.discordGuild
+load_dotenv()
+TOKEN = os.getenv(discordToken)
+GUILD = os.getenv(discordGuild)
+
+client = discord.Client(intents=discord.Intents.default())
 
 #platform routing value
 prv = 'NA1'
@@ -16,3 +31,14 @@ summonerName = 'b3b0'
 summoner = watcherLol.summoner.by_name(prv, summonerName)
 
 print(summoner)
+
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(summoner['summonerLevel'])
+
+client.run(discordToken)
